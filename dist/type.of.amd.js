0299b14c5301a6f5f2a3f6e1d38c564a768340cb
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-06-27 02:30:56 -0300

    build: v0.1.1

--- dist/type.of.amd.js
+++ dist/type.of.amd.js
@@ -8,8 +8,8 @@
  *                       
  * ~ describe-type v0.1.1
  * 
- * @moment Tuesday, June 27, 2017 2:09 AM
- * @commit 4e91e8c11169f4b82c7f1c1492dc75e27ff72250
+ * @moment Tuesday, June 27, 2017 2:26 AM
+ * @commit 61ef95618ae33c112d47a6387322b58f17b01b0f
  * @homepage https://github.com/adriancmiranda/describe-type
  * @author Adrian C. Miranda */
 define(function () { 'use strict';
