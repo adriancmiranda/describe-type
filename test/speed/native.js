bfacfe0cac3f50f0846fa28a8aaebf571b08ddc6
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-07-02 15:55:15 -0300

    test: native methods

---
+++ test/speed/native.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-unused-expressions */
+const { Suite } = require('benchmark');
+const value = /foo/;
+
+new Suite()
+
+.add('typeof value:RegExp', () => {
+	typeof value === 'regexp';
+})
+
+.add('value.constructor === RegExp', () => {
+		value.constructor === RegExp;
+})
+
+.add('Object.prototype.toString.call(value:RegExp)', () => {
+	Object.prototype.toString.call(value) === '[object RegExp]';
+})
+
+.add('value instanceof RegExp', () => {
+	value instanceof RegExp;
+})
+
+.on('cycle', (event) => {
+	console.log(String(event.target));
+})
+
+.on('complete', function () {
+	console.log('\nFastest is ' + this.filter('fastest').map('name'));
+})
+
+.run({ async: true });
