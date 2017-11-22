c2850cb1274a773f734be37959870443ac55a05d
Adrian C. Miranda <adriancmiranda@gmail.com>
2017-11-22 01:52:26 -0200

    build: bump

--- test/unit/as/as.instanceOf.unit.js
+++ test/unit/as/as.instanceOf.unit.js
@@ -2,11 +2,11 @@ import * as type from '~';
 
 // TODO:
 describe('as.instanceOf', function () {
-	it('like method is exposed', function () {
-		expect(type.like).toEqual(jasmine.any(Function));
-	});
+	// it('like method is exposed', function () {
+	// 	expect(type.like).toEqual(jasmine.any(Function));
+	// });
 
-	it('alike method is exposed', function () {
-		expect(type.alike).toEqual(jasmine.any(Function));
-	});
+	// it('alike method is exposed', function () {
+	// 	expect(type.alike).toEqual(jasmine.any(Function));
+	// });
 });
