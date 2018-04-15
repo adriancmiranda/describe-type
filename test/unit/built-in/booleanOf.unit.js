5341098cba691abe1bee44d93b72f790fc7b92fb
Andrey C. Miranda <adriancmiranda@gmail.com>
2018-04-15 15:40:46 -0300

    test: booleanOf desc

--- test/unit/built-in/booleanOf.unit.js
+++ test/unit/built-in/booleanOf.unit.js
@@ -5,35 +5,35 @@ describe('#booleanOf', function () {
 		expect(type.booleanOf).toEqual(jasmine.any(Function));
 	});
 
-	it('', function () {
+	it('0 should be false', function () {
 		expect(type.booleanOf(0)).toEqual(false);
 	});
 
-	it('', function () {
+	it('1 should be true', function () {
 		expect(type.booleanOf(1)).toEqual(true);
 	});
 
-	it('', function () {
+	it('"true" should be true', function () {
 		expect(type.booleanOf('true')).toEqual(true);
 	});
 
-	it('', function () {
+	it('"false" should be false', function () {
 		expect(type.booleanOf('false')).toEqual(false);
 	});
 
-	it('', function () {
+	it('NaN should be false', function () {
 		expect(type.booleanOf(NaN)).toEqual(false);
 	});
 
-	it('', function () {
+	it('Infinity should be true', function () {
 		expect(type.booleanOf(Infinity)).toEqual(true);
 	});
 
-	it('', function () {
+	it('"0" should be false', function () {
 		expect(type.booleanOf('0')).toEqual(false);
 	});
 
-	it('', function () {
+	it('"1" should be true', function () {
 		expect(type.booleanOf('1')).toEqual(true);
 	});
 });
