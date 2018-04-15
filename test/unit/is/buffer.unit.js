ad92cc62753a654505408dbba1ae62dc96531ef0
Andrey C. Miranda <adriancmiranda@gmail.com>
2018-04-15 15:13:25 -0300

    test: add buffer.unit

---
+++ test/unit/is/buffer.unit.js
@@ -0,0 +1,9 @@
+import * as datatypes from 'fixtures/datatypes.fixture.js';
+import * as is from '~/is';
+
+describe('#buffer', () => {
+	it('O método "buffer" deve existir no escopo do módulo "is"', () => {
+		expect(is.buffer).toEqual(jasmine.any(Function));
+	});
+});
+
