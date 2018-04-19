26418a312e1a20bfd8ded7c4aa6fd50d9d1cefd7
Adrian Miranda <adriancmiranda@gmail.com>
2018-04-19 11:54:59 -0300

    fix: babel presets

--- bin/benchmark/index.js
+++ bin/benchmark/index.js
@@ -6,5 +6,5 @@ const spawn = require('../@/spawn');
 const context = typeof argv.c === 'string' ? argv.c : 'test/benchmark/**';
 const files = Array.isArray(argv.f) ? `{${argv.f.join(',')}}` : argv.f || '*';
 sync(resolve(`${join(context, files)}.bench.js`)).forEach(file => {
-  spawn.sync('babel-node', ['--presets', 'env,stage-2', file]);
+  spawn.sync('babel-node', ['--presets', 'env,flow', file]);
 });
--- bin/fixture/index.js
+++ bin/fixture/index.js
@@ -5,5 +5,5 @@ const spawn = require('../@/spawn');
 
 const files = Array.isArray(argv.f) ? `{${argv.f.join(',')}}` : argv.f || '**/*';
 sync(path.resolve(`test/fixtures/${files}?(.fixture).js`)).forEach(file => {
-	spawn.sync('babel-node', ['--presets', 'env', file]);
+	spawn.sync('babel-node', ['--presets', 'env,flow', file]);
 });
