aad15139adf44dd133a3129ea10b2ae259382133
Andrey C. Miranda <adriancmiranda@gmail.com>
2018-04-22 18:29:09 -0300

    test: add stream tests

--- test/unit/is/stream.unit.js
+++ test/unit/is/stream.unit.js
@@ -30,3 +30,65 @@ test('stream.duplex exposure', (t) => {
 test('stream.transform exposure', (t) => {
 	t.is(toString.call(stream.transform), '[object Function]', 'should be a function');
 });
+
+datatypes.stream.iterate(datatype => {
+	test(`${datatype.id} • stream(${datatype.label});`, (t) => {
+		t.is(stream(datatype.value), true, 'should be true');
+	});
+});
+
+datatypes.streamWritable.add(datatypes.streamPassThrough);
+datatypes.streamWritable.add(datatypes.streamTransform);
+datatypes.streamWritable.add(datatypes.streamDuplex);
+datatypes.streamWritable.iterate(datatype => {
+	test(`${datatype.id} • stream.writable(${datatype.label});`, (t) => {
+		t.is(stream.writable(datatype.value), true, 'should be true');
+	});
+});
+datatypes.streamWritable.remove(datatypes.streamPassThrough);
+datatypes.streamWritable.remove(datatypes.streamTransform);
+datatypes.streamWritable.remove(datatypes.streamDuplex);
+
+datatypes.streamReadable.add(datatypes.streamPassThrough);
+datatypes.streamReadable.add(datatypes.streamTransform);
+datatypes.streamReadable.add(datatypes.streamDuplex);
+datatypes.streamReadable.iterate(datatype => {
+	test(`${datatype.id} • stream.readable(${datatype.label});`, (t) => {
+		t.is(stream.readable(datatype.value), true, 'should be true');
+	});
+});
+datatypes.streamReadable.remove(datatypes.streamPassThrough);
+datatypes.streamReadable.remove(datatypes.streamTransform);
+datatypes.streamReadable.remove(datatypes.streamDuplex);
+
+datatypes.streamDuplex.add(datatypes.streamPassThrough);
+datatypes.streamDuplex.add(datatypes.streamTransform);
+datatypes.streamDuplex.iterate(datatype => {
+	test(`${datatype.id} • stream.duplex(${datatype.label});`, (t) => {
+		t.is(stream.duplex(datatype.value), true, 'should be true');
+	});
+});
+datatypes.streamDuplex.remove(datatypes.streamPassThrough);
+datatypes.streamDuplex.remove(datatypes.streamTransform);
+
+datatypes.streamTransform.add(datatypes.streamPassThrough);
+datatypes.streamTransform.iterate(datatype => {
+	test(`${datatype.id} • stream.transform(${datatype.label});`, (t) => {
+		t.is(stream.transform(datatype.value), true, 'should be true');
+	});
+});
+datatypes.streamTransform.remove(datatypes.streamPassThrough);
+
+datatypes.all.remove(datatypes.stream);
+datatypes.all.iterate(datatype => {
+	test(`${datatype.id} • stream(${datatype.label});`, (t) => {
+		t.is(stream(datatype.value), false, 'should be false');
+	});
+	test(`${datatype.id} • stream.writable(${datatype.label});`, (t) => {
+		t.is(stream.writable(datatype.value), false, 'should be false');
+	});
+	test(`${datatype.id} • stream.readable(${datatype.label});`, (t) => {
+		t.is(stream.readable(datatype.value), false, 'should be false');
+	});
+});
+datatypes.all.add(datatypes.stream);
