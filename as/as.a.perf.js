b3de6b382844f7bdc75d23628890e46e411d6a17
Andrey C. Miranda <adriancmiranda@gmail.com>
2018-05-26 21:41:05 -0300

    chore: code style

--- as/as.a.perf.js
+++ as/as.a.perf.js
@@ -1,27 +1,31 @@
 import { Suite } from 'benchmark';
 import { benchmarkFatestStatus, benchmarkCycle } from '../.fixtures/benchmark';
 import * as datatypes from '../.fixtures/datatypes.fixture';
+import { as } from '../index.next';
 import asA from './as.a.next';
 
 let i = 0;
-datatypes.all.iterate((datatype) => {
-	const name = datatype.name;
-	const seal = datatype.seal;
-	const label = datatype.label;
-	const ctor = datatype.ctor;
-	const value = datatype.value;
+datatypes.all.iterate(({ name, seal, label, ctor, value }) => {
 	const loaded = ++i;
 	const total = datatypes.all.size();
 	const progress = Math.round((loaded / total) * 100);
 
 	new Suite()
 
-	.add(`${name}: as(${name}, () => ${label})`, () => {
+	.add(`${name}: asA(${name}, () => ${label})`, () => {
 		asA(ctor, () => value);
 	})
 
-	.add(`${name}: as(${name}, ${label})`, () => {
+	.add(`${name}: asA(${name}, ${label})`, () => {
 		asA(ctor, value);
+	})
+
+	.add(`${name}: as.a(${name}, () => ${label})`, () => {
+		as.a(ctor, () => value);
+	})
+
+	.add(`${name}: as.a(${name}, ${label})`, () => {
+		as.a(ctor, value);
 	})
 
 	.on('cycle', benchmarkCycle())
