a94c2d801f29406584863221f9f68fc7e83168ca
Andrey C. Miranda <adriancmiranda@gmail.com>
2018-06-01 10:05:27 -0300

    test: some patterns

--- internal/patterns.next.js
+++ internal/patterns.next.js
@@ -1,13 +1,13 @@
 // pattern(s)
 export const reIsBase64 = /^(data:\w+\/[a-zA-Z+\-.]+;base64,)?([0-9a-zA-Z+/]{4})*(([0-9a-zA-Z+/]{2}==)|([0-9a-zA-Z+/]{3}=))?$/;
+export const reIsHexadecimal = /^((#|0x)?([0-9A-Fa-f]{6}|[0-9A-Fa-f]{3}))?$/;
+export const reRegExpFlags = /^(?:([gimuy])(?!.*\1)){0,5}$/;
+export const reRegExp = /^\/([\s\S]*)\/((?:([gimuy])(?!.*\3)){0,5})$/;
 export const reFunctionName = /\s*function\s+([^(\s]*)\s*/;
 export const reIsNativeFn = /\[native\scode\]/;
 export const reStringToBoolean = /^true|[1-9]+$/gi;
 export const reToPropName = /^[^a-zA-Z_$]|[^\w|$]|[^\w$]$/g;
 export const reIsHex = /^([A-Fa-f0-9]+|)$/;
-export const reIsHexadecimal = /^((#|0x)?([0-9A-Fa-f]{6}|[0-9A-Fa-f]{3}))?$/;
-export const reRegExpFlags = /^(?:([gimuy])(?!.*\1)){0,5}$/;
-export const reRegExp = /^\/([\s\S]*)\/((?:([gimuy])(?!.*\3)){0,5})$/;
 export const reIsJsonStart = /^\[|^\{(?!\{)/;
 export const reEndsWithBracket = /\]$/;
 export const reEndsWithBrace = /\}$/;
--- internal/patterns.unit.js
+++ internal/patterns.unit.js
@@ -1,6 +1,19 @@
 import test from 'ava';
+import safeRegex from 'safe-regex';
 import * as describeType from '../index.next';
+import * as patterns from './patterns.next';
 
-test('[empty]', (t) => {
-	t.pass();
+test('detect potentially catastrophic exponential-time regular expressions', (t) => {
+	// t.true(safeRegex(patterns.reIsBase64), 'reIsBase64 should be safe');
+	// t.true(safeRegex(patterns.reIsHexadecimal), 'reIsHexadecimal should be safe');
+	// t.true(safeRegex(patterns.reRegExpFlags), 'reRegExpFlags should be safe');
+	// t.true(safeRegex(patterns.reRegExp), 'reRegExp should be safe');
+	t.true(safeRegex(patterns.reFunctionName), 'reFunctionName should be safe');
+	t.true(safeRegex(patterns.reIsNativeFn), 'reIsNativeFn should be safe');
+	t.true(safeRegex(patterns.reStringToBoolean), 'reStringToBoolean should be safe');
+	t.true(safeRegex(patterns.reToPropName), 'reToPropName should be safe');
+	t.true(safeRegex(patterns.reIsHex), 'reIsHex should be safe');
+	t.true(safeRegex(patterns.reIsJsonStart), 'reIsJsonStart should be safe');
+	t.true(safeRegex(patterns.reEndsWithBracket), 'reEndsWithBracket should be safe');
+	t.true(safeRegex(patterns.reEndsWithBrace), 'reEndsWithBrace should be safe');
 });
